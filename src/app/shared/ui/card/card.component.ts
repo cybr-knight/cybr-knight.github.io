c359cd55e1f60abfc88f0a36515b6b150fc51294
Corey Barr <coreybarr01@gmail.com>
2025-12-29 17:49:03 +1100

    Update card and footer

--- src/app/shared/ui/card/card.component.ts
+++ src/app/shared/ui/card/card.component.ts
@@ -1,10 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, input} from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
     selector: 'app-card',
-    imports: [],
+    imports: [
+        NgClass
+    ],
     templateUrl: './card.component.html',
     styleUrl: './card.component.css'
 })
 export class CardComponent {
+    colour = input<"primary" | "secondary" | "tertiary">("primary");
 }
